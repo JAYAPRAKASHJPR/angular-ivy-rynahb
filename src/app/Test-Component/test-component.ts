731e859696163582bb9717112cb3f59e79fd74b4
JAYAPRAKASHJPR <jayaprakashssakthivel@gmail.com>
2021-08-08 20:18:44 +0530

    3 files modified, 2 added

---
+++ src/app/Test-Component/test-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+    <h1>Test component</h1>
+  `,
+  styles: ['color:red']
+})
+export class TestComponent {
+  constructor() {}
+}
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,1 +1,11 @@
-import {NgModule} from ''
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TestComponent } from './Test-Component/test-component';
+const routes: Routes = [
+{path : 'test' , component : TestComponent}
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
